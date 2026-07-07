6a51031d6d264fa00ac52124dda6c9040185267f
lotestudio <info@lote.bg>
2026-07-07 14:38:58 +0300

    fix team slug

--- app/Services/IntegrationService.php
+++ app/Services/IntegrationService.php
@@ -25,11 +25,7 @@ class IntegrationService
 
         $teamSlug = $issue->project->team?->slug ?? 'projects';
 
-        Log::info('teamSlug: '.$teamSlug);
-
         $url = config('app.url')."/{$teamSlug}/{$issue->project->slug}/issues/{$issue->id}";
-
-        Log::info('url: '.$url);
         // $url = config('app.url')."/projects/{$issue->project->slug}/issues/{$issue->id}";
 
         foreach ($integrations as $integration) {
